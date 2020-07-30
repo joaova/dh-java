430b0da5bf1a9f9aea0570b15b1518552f90e79b
joaova <joaovillanova@hotmail.com.br>
2020-07-30 10:29:41 -0300

    pastas

---
+++ aula-07/atividade js/script.js
@@ -0,0 +1,38 @@
+let numA = 5;
+let numB = -5;
+let numC = 0.10;
+let nome = "joao";
+let verdadeiro = true;
+
+console.log(numA);
+console.log(numB);
+console.log(numC);
+console.log(nome);
+console.log(verdadeiro);
+
+let sobrenome = "amaral";
+let nomeInteiro = nome + " " + sobrenome;
+
+console.log(nomeInteiro);
+
+let texto = "joao villanova do amaral";
+
+console.log(text);
+
+valorNulo = null;
+naodefinido = undefined;
+
+console.log(valorNulo, naodefinido);
+
+let frutas = ["banana", "maca", "laranja", "morango", "goiaba"];
+
+console.log(frutas);
+
+let bajulacao = "Digital House blablabla";
+let mes = "a";
+
+switch(mes) {
+    case "a":
+        console.log("maior que um");
+        break;   
+}
